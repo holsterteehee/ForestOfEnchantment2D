8e1e6f694d3011cf5da3d273869d164b93557a9a
Blackcat67919 <jm8956950@gmail.com>
2023-11-12 13:40:38 -0800

    bat enemy
    enemy now follows only at set range

--- Assets/Characters/Scripts/enemy scripts/batFollow.cs
+++ Assets/Characters/Scripts/enemy scripts/batFollow.cs
@@ -6,6 +6,7 @@ public class batFollow : MonoBehaviour
 {
     public float speed;
     private Transform player;
+    public float lineOfSite;
 
     void Start()
     {
@@ -14,6 +15,19 @@ public class batFollow : MonoBehaviour
 
     void Update()
     {
+        float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
+        if(distanceFromPlayer <  lineOfSite)
         transform.position = Vector2.MoveTowards(this.transform.position,player.position,speed*Time.deltaTime);
+
     }
+    
+    //code bellow is a visual to see the range in game
+    
+    /*
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, lineOfSite);
+    }
+    */
 }
