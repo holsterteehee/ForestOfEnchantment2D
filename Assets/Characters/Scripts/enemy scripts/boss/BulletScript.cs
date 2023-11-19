65bde850983b85d89e44f6233505574574cc9f67
Blackcat67919 <jm8956950@gmail.com>
2023-11-19 14:41:48 -0800

    boss place holder
    movement (might need changes) and boss shooting the player.

---
+++ Assets/Characters/Scripts/enemy scripts/boss/BulletScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    GameObject target;
+    public float speed;
+    Rigidbody2D bulletRB;
+
+    void Start()
+    {
+        bulletRB = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player");
+        
+
+        Vector2 moveDir = (target.transform.position - transform.position).normalized * speed;
+        bulletRB.velocity = new Vector2(moveDir.x, moveDir.y);
+        Destroy(gameObject, 2);
+    }
+
+    
+}
--- Assets/Characters/Scripts/enemy scripts/boss/boss1.cs
+++ Assets/Characters/Scripts/enemy scripts/boss/boss1.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class batFollow : MonoBehaviour
+public class boss1 : MonoBehaviour
 {
     Rigidbody2D rb;
     public float speed;
     private Transform player;
     public float lineOfSite;
     public float shootingRange;
-    
-    
+    public float fireRate = 1f;
+    private float nextFireTime;
+    public GameObject bullet;
+    public GameObject bulletParent;
 
     void Start()
     {
@@ -20,11 +22,20 @@ public class batFollow : MonoBehaviour
     void Update()
     {
         float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
-        if(distanceFromPlayer <  lineOfSite)
-        transform.position = Vector2.MoveTowards(this.transform.position,player.position,speed*Time.deltaTime);
+        if(distanceFromPlayer <  lineOfSite && distanceFromPlayer>shootingRange)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position,player.position,speed*Time.deltaTime);
+        }
 
+        else if (distanceFromPlayer<= shootingRange && nextFireTime <Time.time)
+        {
+            Instantiate(bullet,bulletParent.transform.position,Quaternion.identity);
+            nextFireTime = Time.time + fireRate;
+        }
     }
 
+
+    //public below might not be needed
     public void OnHit(int damage, Vector2 knockback)
     {
         rb.velocity = new Vector2(knockback.x, rb.velocity.y + knockback.y);
@@ -37,6 +48,9 @@ public class batFollow : MonoBehaviour
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, lineOfSite);
+        Gizmos.DrawWireSphere(transform.position, shootingRange);
     }
+    
+
     
 }
